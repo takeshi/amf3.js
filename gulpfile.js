5dc0c8d3184740a1dcf152bc84324dc610b72a33
Takeshi Kondo <takeshi.kondo@gmail.com>
2014-12-27 01:28:23 +0900

    update

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,26 @@
 var gulp = require('gulp');
-var browserify = require('gulp-browserify'),
+// var browserify = require('gulp-browserify'),
+var browserify = require('browserify');
+   transform = require('vinyl-transform');
     uglify = require('gulp-uglify'),
-    notify = require( 'gulp-notify'),
+    // notify = require( 'gulp-notify'),
     size = require('gulp-size');
 
 // Basic usage
 gulp.task('default', function() {
 
-  gulp.watch(['amf.js', 'lib/**/*.js'], function(e) {
+  // gulp.watch(['amf.js', 'lib/**/*.js'], function(e) {
+    var browserified = transform(function(filename) {
+      var b = browserify(filename);
+      return b.bundle();
+    });
+    
+
     gulp.src('amf.js')
-        .pipe(browserify({
-          insertGlobals: true
-        }))
+        .pipe(browserified)
         .pipe(uglify())
         .pipe(gulp.dest('./dist'))
-        .pipe(notify('Done!'))
+        // .pipe(notify('Done!'))
         .pipe(size());
-  });
+  // });
 });
